7f5cafd898101fe9907ac1fc3385d47f04467e53
Pedro Lucas <pedro.lucas4431@gmail.com>
2019-12-05 19:16:47 -0300

    att trabfon

--- trabfon.py
+++ trabfon.py
@@ -6,7 +6,9 @@ item = []
 filmes_user = []
 users_sem = []
 cont = []
+ajuda = []
 filmes_users = []
+lista_sem = []
 for i in range(len(u_data)):
     a = u_data[i]
     a = a.split()
@@ -23,14 +25,24 @@ filme = str(input('Digite o filme:'))
 for z in range(len(data)):
     if data[z][1] == filme:
         for u in range(len(filmes_user)):
-                if filmes_user[u] != data[z][1]:
-                    cont.append(data[z][1])
-                    filmes_users.append(data[z][1])
-                    if len(filmes_users) == len(cont):
-                        users_sem = data[z][0]
-                    elif len(filmes_users) > len(cont):
-                        cont.clear()
-                    else:
-                        filmes_users.append(cont[:])
+            if data[z][1] not in filmes_user[u]:
+                cont.append(data[z][1])
+                ajuda.append(data[z][0])
+                if len(filmes_users) <= len(cont):
+                    filmes_users.clear()
+                    filmes_users.append(cont[:])
+                    users_sem.clear()
+                    users_sem.append(ajuda[0])
+                    cont.clear()
+                    ajuda.clear()
+                elif len(users_sem) > len(cont):
+                    cont.clear()
+                    ajuda.clear()
+            else:
+                print('Filme já assistido')
+                break
 
-print(users_sem)
+for r in range(len(data)):
+    if data[r][0] == users_sem[0]:
+        lista_sem.append(data[r])
+print(lista_sem)
