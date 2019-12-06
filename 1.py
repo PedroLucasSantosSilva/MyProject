a9d6fbb55cc237c97439725a36c9423e92650780
Pedro Lucas <pedro.lucas4431@gmail.com>
2019-12-05 23:12:03 -0300

    questão 1

--- 1.py
+++ 1.py
@@ -17,14 +17,3 @@ for l in range(0, linhas):
         if matriz[l][c] % 2 != 0:
             cont += matriz[l][c]
 print(f'A soma dos ímpares é {cont}')
-for l in range(0, linhas):
-    for c in range(0, colunas):
-        if l == 0 and c == 0:
-            maior = matriz[l][c]
-            menor = matriz[l][c]
-        elif matriz[l][c] < menor or menor == 0:
-             menor = matriz[l][c]
-        elif matriz[l][c] > maior:
-             maior = matriz[l][c]
-    print(f'O maior elemento da linha {l + 1} é {maior}')
-    print(f'O menor elemento da linha {l + 1} é {menor}')
