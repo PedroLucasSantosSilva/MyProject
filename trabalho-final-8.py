e83d2628fa95e4820f33b84dc0b8bc2e8b45888d
Pedro Lucas <pedro.lucas4431@gmail.com>
2019-12-14 17:24:22 -0300

    similaridade

--- trabalho-final-8.py
+++ trabalho-final-8.py
@@ -9,7 +9,10 @@ filmes_users = []
 aux_users = []
 contador = []
 similaridade = []
+maximos = []
 cont = 0
+cont2 = 0
+menor = 0
 for i in range(len(u_data)):
     a = u_data[i]
     a = a.split()
@@ -50,9 +53,30 @@ for a in range(len(filmes_users)):
     cont = 0
 for c in range(len(contador)):
     if max(contador) == contador[c]:
-        similaridade.append(contador.index(contador[c]))
-if len(similaridade) == 1:
-    if len(similaridade) == 1:
+        maximos.append(contador.index(contador[c]))
+if len(maximos) == 1:
+    if len(maximos) == 1:
         for d in range(len(data)):
             if data[d][0] == users_film[contador.index(max(contador))] and data[d][1] == filme:
                 print(f'A nota para o filme {filme} de acordo com a similarida é: {data[d][2]}')
+    else:
+        for e in range(len(contador)):
+            for f in range(len(data)):
+                if data[f][0] == users_film[contador.index(contador[f])] and data[f][1] == filme:
+                    for g in range(len(filmes_user)):
+                        for h in range(len(data)):
+                            if data[f][1] == filmes_user[g] and data[h][0] == user and data[f][0] == users_film[contador.index(contador[f])]:
+                                n1 = int(data[h][2])
+                                n2 = int(data[f][2])
+                                cont2 += abs(n1 - n2)
+                            if h == 0:
+                                menor = cont2
+                                similaridade.append(data[f])
+                                cont2 = 0
+                            else:
+                                if cont2 > menor:
+                                    menor = cont2
+                                    similaridade.clear()
+                                    similaridade.append(data[f])
+                                    cont2 = 0
+        print(f'A nota para o filme {filme} de acordo com a similarida é: {data[f][2]}')
